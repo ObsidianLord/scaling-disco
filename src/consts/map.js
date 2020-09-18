7d9bbc69b70787256ad34ea0c1ad8056bdd0c9e7
Gleb Lebedenko <RhubarbDrink@gmail.com>
2020-09-18 23:21:52 +0300

    src: panels: MapPanel.js: rework map with vanilla Leaflet

--- src/consts/map.js
+++ src/consts/map.js
@@ -1,5 +1,10 @@
 export const DEFAULT_MAP_CENTER = [59.9355684, 30.3255795];
 export const DEFAULT_MAP_ZOOM = 16;
 export const DEFAULT_MAP_MAX_ZOOM = 19;
+export const DEFAULT_MAP_ZOOM_CONTROL = false;
 
-export const DEFAULT_TILE_LAYER_URL = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+export const DEFAULT_TILE_URL = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+
+export const DEFAULT_MAP_STYLE_LIGHT = [
+  'grayscale:100%'
+]
--- src/panels/MapPanel.js
+++ src/panels/MapPanel.js
@@ -7,34 +7,30 @@ import {
   PanelHeader,
   Search
 } from '@vkontakte/vkui';
-import {
-  Map,
-  TileLayer,
-  Marker,
-  Popup
-} from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet.tilelayer.colorfilter/src/leaflet-tilelayer-colorfilter.min.js';
 
 import 'leaflet/dist/leaflet.css';
 import '../css/MapPanel.css';
 
 import { SEARCH_BAR_PLACEHOLDER } from '../consts/strings';
 import {
-  DEFAULT_MAP_CENTER,
-  DEFAULT_MAP_ZOOM,
-  DEFAULT_MAP_MAX_ZOOM,
-  DEFAULT_TILE_LAYER_URL
+  DEFAULT_MAP_CENTER as center,
+  DEFAULT_MAP_ZOOM as zoom,
+  DEFAULT_MAP_MAX_ZOOM as maxZoom,
+  DEFAULT_MAP_ZOOM_CONTROL as zoomControl,
+  DEFAULT_TILE_URL,
+  DEFAULT_MAP_STYLE_LIGHT as filter,
 } from '../consts/map';
 
 class MapPanel extends React.Component {
-  constructor(data) {
-    super(data);
+  constructor(props) {
+    super(props);
 
     this.state = {
-      id: data.id,
-      go: data.go
+      id: props.id,
+      go: props.go
     }
-
-    this.searchRef = React.createRef();
   }
 
   componentDidMount() {
@@ -42,21 +38,28 @@ class MapPanel extends React.Component {
     if (search) {
       search.textContent = SEARCH_BAR_PLACEHOLDER;
     }
+
+    const map = L.map('map', {
+      center,
+      maxZoom,
+      zoom,
+      zoomControl
+    });
+  
+    L.tileLayer.colorFilter(
+      DEFAULT_TILE_URL,
+      { filter }
+    ).addTo(map);
   }
 
   render() {
     return (
       <Panel id={this.state.id}>
         <PanelHeader visor={false} />
-        <Map
-          center={DEFAULT_MAP_CENTER}
-          zoom={DEFAULT_MAP_ZOOM}
-          maxZoom={DEFAULT_MAP_MAX_ZOOM}
-        >
-          <TileLayer
-            url={DEFAULT_TILE_LAYER_URL}
-          />
-      </Map>
+        <div
+          id="map"
+          className="map-panel__map-container"
+        ></div>
         <FixedLayout
           vertical="bottom"
           filled
